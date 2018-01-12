99114dc46925f17aec777c2dc278f05f2e501812
Peng Gao <peng.gao.dut@gmail.com>
2018-01-12 16:53:15 +0800

    Upate example
    Signed-off-by: Peng Gao <peng.gao.dut@gmail.com>

--- lazy_skip_list_test.go
+++ lazy_skip_list_test.go
@@ -13,6 +13,21 @@ func Example() {
 	l.Remove(1)
 	fmt.Println(l.Contains(1))
 
+	l.Add(3)
+	l.Add(1)
+	l.Add(2)
+	// iterates list
+	iter := l.Iterator()
+	for {
+		v, ok := iter.Next()
+		if !ok {
+			break
+		}
+		fmt.Println(v.(int))
+	}
 	// Output: true
 	// false
+	// 1
+	// 2
+	// 3
 }
